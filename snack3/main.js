9ca7570b538785ae86601ebfa1010dc57c65d779
Emanuele117 <emanuele.pica7@gmail.com>
2021-10-18 16:06:02 +0200

    inserimento ciclo for

---
+++ snack3/main.js
@@ -0,0 +1,16 @@
+// il software deve chiedere per 10 volte all'utente di inserire un numero.
+
+// il programma stampa la somma di tutti i numeri inseriti.
+
+const sommaNumeri = document.querySelector('.numbers')
+
+for (let index = 0; index < 10; index++) {
+    const element = parseInt(prompt('inserisci un numero'));
+
+    sommaNumeri.innerHTML = element + element
+
+
+
+
+
+}
